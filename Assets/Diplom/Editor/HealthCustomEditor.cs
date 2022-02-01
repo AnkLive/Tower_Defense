fb453213f20eb2939419093fdec43f15e9338644
Resident <bionqine@gmail.com>
2022-02-01 14:38:48 +0200

    fix custom editor

--- Assets/Diplom/Editor/HealthCustomEditor.cs
+++ Assets/Diplom/Editor/HealthCustomEditor.cs
@@ -8,35 +8,69 @@ public class HealthCustomEditor : Editor
 
     public override void OnInspectorGUI()
     {
+        var style = new GUIStyle(GUI.skin.button);
+        style = EditorStyles.wordWrappedLabel;
+        style.normal.textColor = Color.red;
         Health health = (Health)target;
         base.OnInspectorGUI();
         EditorGUILayout.LabelField("Параметры", EditorStyles.boldLabel);
         EditorGUILayout.Space();
-        health._amountOfHealth = EditorGUILayout.FloatField("Здоровье", health._amountOfHealth);
-        health._isShields = EditorGUILayout.Toggle("Щиты", health._isShields);
+        
+        if (health._slider != null && health._sliderFillImage != null && health._sliderBottomImage != null) 
+        {
+            health._amountOfHealth = EditorGUILayout.FloatField("Здоровье", health._amountOfHealth);
+            health._isShields = EditorGUILayout.Toggle("Щиты", health._isShields);
+        }
+        else 
+        {
+            
+            EditorGUILayout.LabelField("Ошибка: требуется указать все необходимые ссылки в дополнительных параметрах",style);
+        }
 
-        if  (health._isShields) health._amountOfShields = EditorGUILayout.FloatField(" ", health._amountOfShields);
-        else health._amountOfShields = 0;
+        if  (health._isShields)
+        {
+            
+            if (health._slider != null && health._sliderFillImage != null && health._sliderBottomImage != null) 
+            {
+                health.SetParemeters(health._currentShields, health._amountOfShields, health._slider, Color.green, Color.blue);
+            }
+            health._amountOfShields = EditorGUILayout.FloatField(" ", health._amountOfShields);
+        }
+        else if (health._slider != null && health._sliderFillImage != null && health._sliderBottomImage != null)
+        {
+            health.SetParemeters(health._currentHealth, health._amountOfHealth, health._slider, Color.red, Color.green);
+            health._amountOfShields = 0;
+            health._currentShields = 0;
+        }
 
         EditorGUILayout.Space();
         EditorGUILayout.LabelField("Дополнительные параметры", EditorStyles.boldLabel);
         EditorGUILayout.Space();
-        EditorGUILayout.LabelField("Ссылка на слайдер");
-        EditorGUILayout.ObjectField(health._healthSlider, typeof(Slider), true);
-
+        health._slider = (Slider)EditorGUILayout.ObjectField("Ссылка на слайдер", health._slider, typeof(Slider), true);
+        health._sliderFillImage = (Image)EditorGUILayout.ObjectField("Fill Slider", health._sliderFillImage, typeof(Image), true);
+        health._sliderBottomImage = (Image)EditorGUILayout.ObjectField("Bottom Slider", health._sliderBottomImage, typeof(Image), true);
         EditorGUILayout.Space();
-        EditorGUILayout.LabelField("Показать дополнительные данные", EditorStyles.boldLabel);
-        health._isAdditionalSettings = EditorGUILayout.Toggle(health._isAdditionalSettings);
-        EditorGUILayout.Space();
-
-        if (health._isAdditionalSettings) 
+        
+        if (health._slider != null && health._sliderFillImage != null && health._sliderBottomImage != null) 
         {
-            EditorGUILayout.LabelField("Текущее показатели", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Показать дополнительные данные", EditorStyles.boldLabel);
+            health._isAdditionalSettings = EditorGUILayout.Toggle(health._isAdditionalSettings);
             EditorGUILayout.Space();
-            health._currentHealth = EditorGUILayout.FloatField("Здоровье", health._currentHealth);
 
-            if  (health._isShields) health._currentShields = EditorGUILayout.FloatField("Щиты", health._currentShields);
-            health._isDied = EditorGUILayout.Toggle("Враг уничтожен", health._isDied);
+            if (health._isAdditionalSettings) 
+            {
+                EditorGUILayout.LabelField("Текущее показатели", EditorStyles.boldLabel);
+                EditorGUILayout.Space();
+                health._currentHealth = EditorGUILayout.FloatField("Здоровье", health._currentHealth);
+
+                if  (health._isShields) health._currentShields = EditorGUILayout.FloatField("Щиты", health._currentShields);
+                else 
+                {
+                    health._amountOfShields = 0;
+                    health._currentShields = 0;
+                }
+                health._isDied = EditorGUILayout.Toggle("Враг уничтожен", health._isDied);
+            }
         }
         serializedObject.ApplyModifiedProperties();
         
--- Assets/Diplom/Resources/Prefabs/Scripts/Health.cs
+++ Assets/Diplom/Resources/Prefabs/Scripts/Health.cs
@@ -15,22 +15,51 @@ public class Health : MonoBehaviour
     [field: SerializeField, HideInInspector]
     public float _currentShields { get; set; }
     [field: SerializeField, HideInInspector]
-    public Slider _healthSlider;
+    public Slider _slider { get; set; }
     [field: SerializeField, HideInInspector]
     public bool _isDied { get; set; } = false;
-
-    void Start()
+    [field: SerializeField, HideInInspector]
+    public Image _sliderFillImage { get; set; }
+    [field: SerializeField, HideInInspector]
+    public Image _sliderBottomImage { get; set; }
+ 
+    private void Awake()
     {
-        _currentHealth = _amountOfHealth;
-        _healthSlider.maxValue = _amountOfHealth;
-        _healthSlider.value = _amountOfHealth;
+        if (_isShields) SetParemeters(_currentShields, _amountOfShields, _slider, Color.green, Color.blue);
+        else SetParemeters(_currentHealth, _amountOfHealth, _slider, Color.red, Color.green);
     }
 
     public void TakeDamage(float damage)
     {
-        _currentHealth -= damage;
-        _healthSlider.value = _currentHealth;
 
         if (_currentHealth <= 0)  _isDied = true;
+        else 
+        {
+
+            if (_isShields) 
+            {
+                _currentShields -= damage;
+
+                if (_currentShields <= 0) 
+                {
+                    SetParemeters(_currentHealth, _amountOfHealth, _slider, Color.red, Color.green);
+                    _isShields = false;
+                }
+            }
+            else 
+            {
+                _currentHealth -= damage;
+                _slider.value = _currentHealth;
+            }
+        }
+    }
+
+    public void SetParemeters(float current, float amount, Slider slider, Color a, Color b) 
+    {
+        current = amount;
+        slider.maxValue = amount;
+        slider.value = amount;
+        _sliderFillImage.color = a;
+        _sliderBottomImage.color = b;
     }
 }
